d5aada976e34a6357c48488d29ba8137d281cb3f
Yue Jiao <cleamoon@hotmail.com>
2026-02-09 19:55:35 +0100

    Remove unused code

--- src/entry.ts
+++
@@ -1,3 +0,0 @@
-import { buildSite } from './generator/build'
-
-buildSite()
--- src/generator/render.ts
+++
@@ -1,7 +0,0 @@
-import { h } from './runtime/h'
-import { renderToHtml } from './runtime/renderToHtml'
-
-export function renderPage(route: { path: string, component: any}): string {
-    const content = route.component()
-    return `<!DOCTYPE html><html><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1"/><title>Document</title></head><body>${renderToHtml(content)}</body></html>`
-}
